ed62d49d9093e036ed0b4568f026067d4261df4d
Java-Instructor <143517387+Java-instructor@users.noreply.github.com>
2023-09-19 19:42:07 +0530

    RetailManagement demo application updated

---
+++ src/main/java/com/java/instructor/spring/microservice/retail/SpringMicroserviceRetailManagementApplication.java
@@ -0,0 +1,13 @@
+package com.java.instructor.spring.microservice.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceRetailManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceRetailManagementApplication.class, args);
+	}
+
+}
